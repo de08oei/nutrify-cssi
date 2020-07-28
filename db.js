2bede2ae4624399e9ae239c0cdaa31e228c79a65
Surbhi Gulati <53664026+surbhi-gulati@users.noreply.github.com>
2020-07-28 16:42:48 -0700

    Add database

--- db.js
+++ db.js
@@ -1,12 +1,24 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
-const Coordinates = new mongoose.Schema({
-	x: Number,
-	y: Number
+const Recipe = new mongoose.Schema({
+	Recipe_Name: String,
+	Recipe_Allergens: Array,
+	Recipe_Ingredients: Array,
+	Recipe_Procedure: Array,
+	Recipe_LowTime: Number,
+	Recipe_HighTime: Number,
+	Recipe_Cuisine: String
 });
 
-mongoose.model("Coordinates", Coordinates);
+const User = new mongoose.Schema({
+	Token: String,
+	Fridge: Array,
+	Liked_Recipes: Array
+});
+
+mongoose.model("Recipe", Recipe);
+mongoose.model("User", User);
 
 
 mongoose.connect("http://localhost:3000", function (err, res){
